f1c9385b265d16ef0ae928b818eb1cb708332b4c
Jayesh Poonia <103627572+Jayesh260@users.noreply.github.com>
2024-06-30 21:16:07 +0530

    Create 1.cpp

---
+++ codeforces/EPIC Institute of Technology Round Summer 2024/1.cpp
@@ -0,0 +1,23 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int lli;
+const int mod=1e9 +7;
+#define rng_init mt19937 rng(chrono::steady_clock::now().time_since_epoch().count())
+#define rng_seed(x) mt19937 rng(x)
+#define sz(x) (int)(x).size()
+const int MXN =1e5+5, INF=1e9+5; 
+void solve(){
+    int n,k;
+    cin>>n>>k;
+    cout<<n*k-(k-1)<<endl;
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    int t=1;
+    cin>>t;
+    while(t--)
+    solve();
+    return 0;
+}
